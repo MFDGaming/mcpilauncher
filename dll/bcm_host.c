9cc8b05574fea362d6fb02ce7d4effe47367323d
mhsjlw <mhsjlw@users.noreply.github.com>
2018-05-18 19:44:59 -0400

    Organize - Remove QEMU from source tree - Switch to CMake build system - Prepare to remote natives folder - Add CMake script that automates everything (including downloading) - Switch to a `dlload`-system in QEMU, allowing changes to be made to trampoline and not require re-compiling of QEMU

---
+++ dll/bcm_host.c
@@ -0,0 +1,9 @@
+#include "dll.h"
+
+FUNCTION_0(bcm_host_init)
+FUNCTION_0(bcm_host_deinit)
+FUNCTION_0(graphics_get_display_size)
+FUNCTION_6(vc_dispmanx_element_add)
+FUNCTION_1(vc_dispmanx_display_open)
+FUNCTION_1(vc_dispmanx_update_start)
+FUNCTION_1(vc_dispmanx_update_submit_sync)
